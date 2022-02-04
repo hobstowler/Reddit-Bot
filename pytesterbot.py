961037d19f15a48373980482ec66c81fc55e5f17
Hobs Towler <towlerj@oregonstate.edu>
2022-02-03 23:16:47 -0500

    credentials to separate file

--- pytesterbot.py
+++ pytesterbot.py
@@ -1,12 +1,30 @@
 from platform import python_branch
 import auth
 import praw
+import json
 
 class RedditBot:
-    def __init__(self, client_id: str, secret: str, refresh_token: str = None) -> None:
-        self._client_id = client_id
-        self._secret = secret
-        self._refresh_token = refresh_token
+    def __init__(self, credentials_file=None) -> None:
+        if credentials_file is None:
+            credentials_file = "credentials.txt"
+        with open(credentials_file, 'r') as file:
+            credentials = json.load(file)
+            self._client_id = credentials.get('client_id')
+            self._secret = credentials.get('secret')
+            self._refresh_token = credentials.get('refresh_token')
+
+    def refresh_credentials(self) -> None:
+        credentials = {}
+        if self._client_id is not None:
+            credentials.update({'client_id': self._client_id})
+        if self._secret is not None:
+            credentials.update({'secret': self._secret})
+        if self._refresh_token is not None:
+            credentials.update({'refresh_token': self._refresh_token})
+
+        with open('credentials.txt', 'w') as file:
+            json.dump(credentials, file)
+        
 
     def refresh_auth_token(self) -> None:
         return auth.get_refresh_token(self._client_id, self._secret)
@@ -16,7 +34,6 @@ class RedditBot:
             client_id = self._client_id,
             client_secret = self._secret,
             refresh_token = self._refresh_token,
-            # redirect_uri = 'http://localhost:8080',
             user_agent = 'test by u/pytesterbot'
         )
 
@@ -32,6 +49,7 @@ class RedditBot:
                 posts_of_interest.append(submission)
                 print(submission.score, ":", submission.author, ":", submission.title)
         print(len(posts_of_interest))
+
 
 class Post:
     """
@@ -59,5 +77,6 @@ class User:
 
 
 
-py_bot = PyTesterBot('ehR39skBNNOdtRnTQIk3pw', 'uZ0M43AxH0TF_z6j6jVkXpXIMsqsJw', '1512276763749-lHJIeRUXXuWz4Dtz6YcC4wAS2YPW-Q')
+py_bot = RedditBot()
 py_bot.get_posts('wallstreetbets')
+#py_bot.refresh_credentials()
