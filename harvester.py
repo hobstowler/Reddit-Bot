f99f49f5460aab519d94c3d1af7c68b38f862517
Hobs Towler <towlerj@oregonstate.edu>
2022-02-04 16:48:27 -0500

    methods to harvester and add parser.py

--- harvester.py
+++ harvester.py
@@ -1,21 +1,30 @@
 from platform import python_branch
+from os.path import exists
 import auth
 import praw
 import json
+import pickle
 
 class RedditBot:
-    def __init__(self, credentials_file:str=None) -> None:
+    def __init__(self, credentials_file:str=None, data_file:str=None) -> None:
+        self._default_save_file = 'data.p'
         self._users = {}
         if credentials_file is None:
-            self._credentials_file = "credentials.txt"
-        else:
-            self._credentials_file = credentials_file
+            credentials_file = "credentials.txt"
 
-        with open(self._credentials_file, 'r') as file:
+        with open(credentials_file, 'r') as file:
             credentials = json.load(file)
+            self._credentials = credentials
             self._client_id = credentials.get('client_id')
             self._secret = credentials.get('secret')
             self._refresh_token = credentials.get('refresh_token')
+
+        # handle default better
+        self._data = self.load_data(data_file)
+        if self._data is not None:
+            self._users = self._data.get('users')
+            print("loaded users")
+        
 
     def refresh_credentials(self) -> None:
         credentials = {}
@@ -32,21 +41,19 @@ class RedditBot:
     def refresh_auth_token(self) -> None:
         return auth.get_refresh_token(self._client_id, self._secret)
 
-    def get_reddit(self) -> praw.Reddit:
+    def get_reddit(self, agent_text:str) -> praw.Reddit:
         return praw.Reddit(
             client_id = self._client_id,
             client_secret = self._secret,
-            user_agent = 'test by u/pytesterbot'
+            user_agent = agent_text
         )
 
-    def get_posts(self, subreddit_name: str, number: int = 5) -> list:
-        reddit = self.get_reddit()
-        subreddit = reddit.subreddit('all').top('hour')
+    def get_posts(self, subreddit_name: str, number: int = 25) -> list:
+        reddit = self.get_reddit('test getting posts by u/pytesterbot')
+        subreddit = reddit.subreddit('all').top('hour', limit=number)
 
         for submission in subreddit:
-            #print(submission.title)
-            if submission.score > 200:
-                print("found post.")
+            if submission.score > 100:
                 author = submission.author.name
                 id = submission.author.id
                 new_post = Post(author, submission.subreddit.name, submission.id, {})
@@ -58,19 +65,44 @@ class RedditBot:
                 else:
                     self._users.get(id).add_post(new_post)
         
-        for id,user in self._users.items():
-            print(id, ":", user.get_name())
         print("Finished.")
+
+    def get_comments(self, post_url: str):
+        reddit = self.get_reddit('test getting comments by u/pytesterbot')
+        post = reddit.submission(url=post_url)
+        print("success?", post.title)
+    
+    def save_data(self, filename: str = None):
+        if filename is None:
+            filename = self._default_save_file
+        print(filename)
+        data = {
+            'users': self._users,
+            'credentials': self._credentials
+            }
+        with open(filename, 'wb') as outfile:
+            pickle.dump(data, outfile)
+
+
+    def load_data(self, filename: str = None):
+        if filename is None:
+            filename = self._default_save_file
+        if exists(filename):
+            with open(filename, 'rb') as infile:
+                return pickle.load(infile)
+
 
     def dump_to_csv(self):
         with open('raw_data.csv', 'w') as file:
-            header = "user, user_id, subreddit, post_id \n"
+            print("writing to csv")
+            header = "username, user_id, # posts, subreddit, post_id \n"
             file.write(header)
             for user in self._users.values():
                 for post in user.get_posts().values():
                     print(user.get_name())
                     file.write(user.get_name() + ",")
                     file.write(user.get_id() + ",")
+                    file.write(str(len(user.get_posts())) + ",")
                     file.write(post.subreddit + ",")
                     file.write(post.id + "\n")
 
@@ -84,6 +116,11 @@ class Post:
         self.subreddit = subreddit
         self.id = id
         self.stats = stats
+
+
+class Comment:
+    def __init__(self) -> None:
+        pass
 
 
 class User:
@@ -109,7 +146,15 @@ class User:
         return self._posts
 
 
+def main():
+    py_bot = RedditBot()
+    py_bot.get_posts('wallstreetbets')
+    py_bot.save_data()
+    py_bot.dump_to_csv()
+    py_bot.get_posts('politics')
+    py_bot.save_data()
+    py_bot.dump_to_csv()
 
-py_bot = RedditBot()
-py_bot.get_posts('wallstreetbets')
-py_bot.dump_to_csv()
+
+if __name__ == "__main__":
+    main()
---
+++ parser.py
@@ -0,0 +1,31 @@
+from harvester import *
+
+
+class KeyWordAnalyzer:
+    def __init__(self) -> None:
+        self._bot = RedditBot()
+        self._threshold = self.set_threshold()
+        keyword_lists = self.load_lists()
+
+    def load_lists(self):
+        # load keywords
+        # load exceptions
+        # load special words
+        pass
+
+    def save_keywords(self):
+        pass
+
+    def set_threshold(self, threshold: int = 5):
+        """[summary]
+        Sets the count threshold under which a key word is not considered relevant.
+        Args:
+            val (int): [description]
+        """
+        self._threshold = threshold
+
+    def extract_comments_from_post(self, post_url: str):
+        self._bot.get_comments('https://old.reddit.com/r/Virginia/comments/skdtww/glenn_youngkin_set_up_a_tip_line_to_snitch_on/')
+
+keybot = KeyWordAnalyzer()
+keybot.extract_comments_from_post(None)
